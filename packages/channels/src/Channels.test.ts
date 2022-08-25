26d4c7a575c42c3360f2a17f5435826232f09f5f
Peter van der Noord <peterdunord@gmail.com>
2022-08-25 21:32:54 +0200

    Add some tests

--- packages/channels/src/Channels.test.ts
+++ packages/channels/src/Channels.test.ts
@@ -15,6 +15,10 @@ it('initializes', () => {
 const getAudioGraph = (channels: Channels) =>
   (channels.audioContext as any).toJSON();
 
+// const logJson = (json: any) => {
+//   console.log(JSON.stringify(json, null, 2));
+// };
+
 describe('Channels', () => {
   let channelsInstance: Channels;
 
@@ -26,12 +30,76 @@ describe('Channels', () => {
   });
 
   it('creates main volume nodes', () => {
-    console.log(getAudioGraph(channelsInstance));
-  });
-  it('adds a channel', () => {
-    const channel = channelsInstance?.createChannel('name');
+    const destinationNode = getAudioGraph(channelsInstance);
+    const fadeNode = destinationNode.inputs[0];
+    const gainNode = fadeNode.inputs[0];
 
-    expect(channel).toBeInstanceOf(Channel);
-    expect(channel.volumeNodes).toBeInstanceOf(VolumeNodes);
+    expect(gainNode.name).toBe('GainNode');
+    expect(fadeNode.name).toBe('GainNode');
+    expect(gainNode.gain.value).toBe(1);
+    expect(fadeNode.gain.value).toBe(1);
+    expect(destinationNode.inputs.length).toBe(1);
+    expect(fadeNode.inputs.length).toBe(1);
+    expect(gainNode.inputs.length).toBe(0);
+  });
+  it('sets main volume', () => {
+    channelsInstance.setVolume(0.5);
+    const destinationNode = getAudioGraph(channelsInstance);
+    const fadeNode = destinationNode.inputs[0];
+    const gainNode = fadeNode.inputs[0];
+
+    expect(gainNode.gain.value).toBe(0.5);
+    expect(fadeNode.gain.value).toBe(1);
+  });
+  it('mutes main volume', () => {
+    channelsInstance.mute();
+    const destinationNode = getAudioGraph(channelsInstance);
+    const fadeNode = destinationNode.inputs[0];
+    const gainNode = fadeNode.inputs[0];
+
+    expect(gainNode.gain.value).toBe(0);
+    expect(fadeNode.gain.value).toBe(1);
+  });
+  it('restores volume after unmuting', () => {
+    channelsInstance.setVolume(0.5);
+    channelsInstance.mute();
+    channelsInstance.unmute();
+    const destinationNode = getAudioGraph(channelsInstance);
+    const fadeNode = destinationNode.inputs[0];
+    const gainNode = fadeNode.inputs[0];
+
+    expect(gainNode.gain.value).toBe(0.5);
+    expect(fadeNode.gain.value).toBe(1);
+  });
+  describe('Channel creation', () => {
+    it('creates a channel', () => {
+      const channel = channelsInstance?.createChannel('name');
+      expect(channel).toBeInstanceOf(Channel);
+      expect(channel.volumeNodes).toBeInstanceOf(VolumeNodes);
+    });
+
+    it('creates and connects volume nodes for channel', () => {
+      channelsInstance?.createChannel('name');
+
+      const destinationNode = getAudioGraph(channelsInstance);
+      const mainFadeNode = destinationNode.inputs[0];
+      const mainGainNode = mainFadeNode.inputs[0];
+      const channelFadeNode = mainGainNode.inputs[0];
+      const channelGainNode = channelFadeNode.inputs[0];
+      expect(mainGainNode.name).toBe('GainNode');
+      expect(mainFadeNode.name).toBe('GainNode');
+      expect(channelFadeNode.name).toBe('GainNode');
+      expect(channelGainNode.name).toBe('GainNode');
+      expect(mainGainNode.gain.value).toBe(1);
+      expect(mainFadeNode.gain.value).toBe(1);
+      expect(channelFadeNode.gain.value).toBe(1);
+      expect(channelGainNode.gain.value).toBe(1);
+
+      expect(destinationNode.inputs.length).toBe(1);
+      expect(mainFadeNode.inputs.length).toBe(1);
+      expect(mainGainNode.inputs.length).toBe(1);
+      expect(channelFadeNode.inputs.length).toBe(1);
+      expect(channelGainNode.inputs.length).toBe(0);
+    });
   });
 });
