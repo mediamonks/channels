8d6f7daa12787afe4c8ec9e409d580efb27ad040
Peter van der Noord <peterdunord@gmail.com>
2022-08-25 16:55:57 +0200

    Update tests

--- packages/channels/src/Channels.test.ts
+++ packages/channels/src/Channels.test.ts
@@ -1,13 +1,37 @@
 import { Channels } from './Channels';
 import 'web-audio-test-api';
+import { Channel } from './Channel';
+import { VolumeNodes } from './VolumeNodes';
+
+it('initializes', () => {
+  const channels = new Channels({
+    soundsPath: '',
+    soundsExtension: '',
+  });
+
+  expect(channels).toBeInstanceOf(Channels);
+});
+
+const getAudioGraph = (channels: Channels) =>
+  (channels.audioContext as any).toJSON();
 
 describe('Channels', () => {
-  it('initializes', () => {
-    const channels = new Channels({
+  let channelsInstance: Channels;
+
+  beforeEach(() => {
+    channelsInstance = new Channels({
       soundsPath: 'path',
       soundsExtension: 'mp3',
     });
+  });
 
-    expect(channels).toBeInstanceOf(Channels);
+  it('creates main volume nodes', () => {
+    console.log(getAudioGraph(channelsInstance));
+  });
+  it('adds a channel', () => {
+    const channel = channelsInstance?.createChannel('name');
+
+    expect(channel).toBeInstanceOf(Channel);
+    expect(channel.volumeNodes).toBeInstanceOf(VolumeNodes);
   });
 });
