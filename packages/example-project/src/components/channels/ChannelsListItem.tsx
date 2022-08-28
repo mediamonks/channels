f861debc2cd2451fedea8f21a12367db56cd86db
Peter van der Noord <peterdunord@gmail.com>
2022-08-28 22:05:40 +0200

    Hide empty options object on channel item

--- packages/example-project/src/components/channels/ChannelsListItem.tsx
+++ packages/example-project/src/components/channels/ChannelsListItem.tsx
@@ -29,7 +29,12 @@ export const ChannelsListItem = ({ channel }: Props) => {
         {channel.type === 'monophonic' ? <small> (monophonic)</small> : null}
       </h3>
       <p>
-        <small>{JSON.stringify(channel.defaultPlayStopOptions, null, 2)}</small>
+        {channel.defaultPlayStopOptions !== undefined &&
+          Object.keys(channel.defaultPlayStopOptions).length > 0 && (
+            <small>
+              {JSON.stringify(channel.defaultPlayStopOptions, null, 2)}
+            </small>
+          )}
       </p>
       <button onClick={() => channelsInstance.stopAll({ channel })}>
         stop all sounds on this channel
