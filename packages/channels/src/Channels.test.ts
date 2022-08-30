04084f2ba1cbe6bf7f245b1765bedd500a5416b2
Peter van der Noord <peterdunord@gmail.com>
2022-08-30 13:15:08 +0200

    Add a test

--- packages/channels/src/Channels.test.ts
+++ packages/channels/src/Channels.test.ts
@@ -86,6 +86,16 @@ describe('Channels instance', () => {
         expect(fadeNode.inputs.length).toBe(1);
         expect(gainNode.inputs.length).toBe(0);
       });
+      it('Has default volume', () => {
+        const destinationNode = getAudioGraph(channelsInstance);
+        const fadeNode = destinationNode.inputs[0];
+        const gainNode = fadeNode.inputs[0];
+
+        expect(gainNode.gain.value).toBe(1);
+        expect(fadeNode.gain.value).toBe(1);
+        expect(channelsInstance.getVolume()).toBe(1);
+        expect(channelsInstance.getFadeVolume()).toBe(1);
+      });
       it('Sets volume', () => {
         channelsInstance.setVolume(0.5);
         const destinationNode = getAudioGraph(channelsInstance);
@@ -95,6 +105,7 @@ describe('Channels instance', () => {
         expect(gainNode.gain.value).toBe(0.5);
         expect(fadeNode.gain.value).toBe(1);
         expect(channelsInstance.getVolume()).toBe(0.5);
+        expect(channelsInstance.getFadeVolume()).toBe(1);
       });
       it('dispatches an event when setting main volume', () => {
         const listener = jest.fn();
