c0f2520f96c07677fa9137ab33babf7d7ec14064
Peter van der Noord <peterdunord@gmail.com>
2022-09-06 12:23:14 +0200

    Add test for effects on main output (#41)

--- packages/channels/src/Channels.test.ts
+++ packages/channels/src/Channels.test.ts
@@ -3,6 +3,8 @@ import 'web-audio-test-api';
 import SampleManager from 'sample-manager';
 import { mockXMLHttpRequest } from './test/mockXMLHttpRequest';
 import { mockChannelsInstance } from './test/mockChannelsInstance';
+import { getAudioGraph } from './test/getAudioGraph';
+import { AudioContext } from './util/audioContext';
 
 mockXMLHttpRequest();
 
@@ -47,5 +49,76 @@ describe('Channels instance', () => {
         channelsInstance.sampleManager.getSampleByName('sound').audioBuffer
       ).toBeInstanceOf(AudioBuffer);
     });
+  });
+  it('adds preVolume effect', () => {
+    const context = new AudioContext();
+    const filter = context.createBiquadFilter();
+    const channelsInstance = new Channels({
+      soundsPath: 'path',
+      soundsExtension: 'mp3',
+      audioContext: context,
+      effects: {
+        preVolume: { input: filter, output: filter },
+      },
+    });
+
+    channelsInstance.createChannel('channel');
+
+    const destinationNode = getAudioGraph(channelsInstance);
+    const mainFadeNode = destinationNode.inputs[0];
+    const mainGainNode = mainFadeNode.inputs[0];
+    const filterNode = mainGainNode.inputs[0];
+    const channelFadeNode = filterNode.inputs[0];
+    const channelGainNode = channelFadeNode.inputs[0];
+
+    expect(filterNode.name).toBe('BiquadFilterNode');
+    expect(channelFadeNode.name).toBe('GainNode');
+    expect(channelGainNode.name).toBe('GainNode');
+  });
+  it('adds postVolume effect', () => {
+    const context = new AudioContext();
+    const filter = context.createBiquadFilter();
+    const channelsInstance = new Channels({
+      soundsPath: 'path',
+      soundsExtension: 'mp3',
+      audioContext: context,
+      effects: {
+        postVolume: { input: filter, output: filter },
+      },
+    });
+
+    const destinationNode = getAudioGraph(channelsInstance);
+    const filterNode = destinationNode.inputs[0];
+    const mainFadeNode = filterNode.inputs[0];
+    const mainGainNode = mainFadeNode.inputs[0];
+
+    expect(filterNode.name).toBe('BiquadFilterNode');
+    expect(mainFadeNode.name).toBe('GainNode');
+    expect(mainGainNode.name).toBe('GainNode');
+  });
+  it('adds post and preVolume effect', () => {
+    const context = new AudioContext();
+    const filter = context.createBiquadFilter();
+    const convolver = context.createConvolver();
+    const channelsInstance = new Channels({
+      soundsPath: 'path',
+      soundsExtension: 'mp3',
+      audioContext: context,
+      effects: {
+        postVolume: { input: filter, output: filter },
+        preVolume: { input: convolver, output: convolver },
+      },
+    });
+
+    const destinationNode = getAudioGraph(channelsInstance);
+    const filterNode = destinationNode.inputs[0];
+    const mainFadeNode = filterNode.inputs[0];
+    const mainGainNode = mainFadeNode.inputs[0];
+    const convolverNode = mainGainNode.inputs[0];
+
+    expect(filterNode.name).toBe('BiquadFilterNode');
+    expect(mainFadeNode.name).toBe('GainNode');
+    expect(mainGainNode.name).toBe('GainNode');
+    expect(convolverNode.name).toBe('ConvolverNode');
   });
 });
