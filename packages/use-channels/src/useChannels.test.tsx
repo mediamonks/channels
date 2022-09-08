b4df150f944f70b588cb2484ac5f9310b43e3deb
Peter van der Noord <peterdunord@gmail.com>
2022-09-08 16:41:05 +0200

    Add simple test for useVolumeChange and usePanningChange

--- packages/use-channels/src/useChannels.test.tsx
+++ packages/use-channels/src/useChannels.test.tsx
@@ -1,34 +1,107 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { ChannelsProvider, useChannels } from './useChannels';
 import 'web-audio-test-api';
-import { Channels } from '@mediamonks/channels';
+import { Channel, Channels } from '@mediamonks/channels';
+import { ReactNode, useRef, useState } from 'react';
+import { useVolumeChange } from './useVolumeChange';
+import { usePanningChange } from './usePanningChange';
 
 (window as any).WebAudioTestAPI.setState({
   'AudioContext#createStereoPanner': 'enabled',
 });
 
-const ChildComponent = () => {
+const ComponentUsingChannels = () => {
   const channelsInstance = useChannels();
-  console.log();
+
   return (
-    <div data-testid="channels-exists">
-      {channelsInstance && channelsInstance instanceof Channels
-        ? 'true'
-        : 'false'}
-    </div>
+    <>
+      <div data-testid="channels-exists">
+        {channelsInstance && channelsInstance instanceof Channels
+          ? 'true'
+          : 'false'}
+      </div>
+    </>
   );
 };
-const TestComponent = () => {
+
+const ComponentThatChangesMainVolume = () => {
+  const channels = useChannels();
+  const channelRef = useRef<Channel>();
+
+  const [volume, setVolume] = useState(channels.getVolume);
+
+  useVolumeChange({
+    onChange: value => {
+      setVolume(value);
+    },
+    target: channelRef.current,
+  });
+
+  const onClick = () => {
+    channels.setVolume(0.5);
+  };
+
+  return (
+    <>
+      <button onClick={onClick}>set volume channel</button>
+      <div data-testid="volume">{volume}</div>
+    </>
+  );
+};
+
+const ComponentThatChangesMainPan = () => {
+  const channels = useChannels();
+  const [pan, setPan] = useState(channels.getPan());
+
+  usePanningChange({
+    onChange: value => {
+      setPan(value);
+    },
+  });
+
+  return (
+    <>
+      <button onClick={() => channels.setPan(-1)}>set volume pan</button>
+      <div data-testid="pan">{pan}</div>
+    </>
+  );
+};
+
+const TestProviderComponent = ({ children }: { children: ReactNode }) => {
   return (
     <ChannelsProvider soundsExtension={'mp3'} soundsPath={'path/'}>
-      <ChildComponent />
+      {children}
     </ChannelsProvider>
   );
 };
 
 describe('useChannels', () => {
   it('Provides a channel instance', () => {
-    const { getByTestId } = render(<TestComponent />);
+    const { getByTestId } = render(
+      <TestProviderComponent>
+        <ComponentUsingChannels />
+      </TestProviderComponent>
+    );
     expect(getByTestId('channels-exists').textContent).toBe('true');
+  });
+  it('Listens to main volume change', () => {
+    const { getByRole, getByTestId } = render(
+      <TestProviderComponent>
+        <ComponentThatChangesMainVolume />
+      </TestProviderComponent>
+    );
+    expect(getByTestId('volume').textContent).toBe('1');
+    fireEvent.click(getByRole('button'));
+    expect(getByTestId('volume').textContent).toBe('0.5');
+  });
+  it('Listens to main pan change', () => {
+    const { getByRole, getByTestId } = render(
+      <TestProviderComponent>
+        <ComponentThatChangesMainPan />
+      </TestProviderComponent>
+    );
+    expect(getByTestId('pan').textContent).toBe('0');
+    fireEvent.click(getByRole('button'));
+    expect(getByTestId('pan').textContent).toBe('-1');
   });
 });
