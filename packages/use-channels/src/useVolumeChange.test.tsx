7b500292b85ce22bffb4654b7253aabd853e3320
Peter van der Noord <peterdunord@gmail.com>
2022-09-08 17:47:36 +0200

    Use proper test for useVolumeChange

--- packages/use-channels/src/useVolumeChange.test.tsx
+++ packages/use-channels/src/useVolumeChange.test.tsx
@@ -1,56 +1,38 @@
-import { fireEvent, render } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { ChannelsProvider, useChannels } from './useChannels';
 import 'web-audio-test-api';
-import { Channel } from '@mediamonks/channels';
-import { ReactNode, useRef, useState } from 'react';
+import { Channels } from '@mediamonks/channels';
+import { ReactNode } from 'react';
 import { useVolumeChange } from './useVolumeChange';
 
 (window as any).WebAudioTestAPI.setState({
   'AudioContext#createStereoPanner': 'enabled',
 });
 
-const ComponentThatChangesMainVolume = () => {
-  const channels = useChannels();
-  const channelRef = useRef<Channel>();
-
-  const [volume, setVolume] = useState(channels.getVolume);
-
-  useVolumeChange({
-    onChange: value => {
-      setVolume(value);
-    },
-    target: channelRef.current,
-  });
-
-  const onClick = () => {
-    channels.setVolume(0.5);
-  };
-
-  return (
-    <>
-      <button onClick={onClick}>set volume channel</button>
-      <div data-testid="volume">{volume}</div>
-    </>
-  );
-};
-
-const TestProviderComponent = ({ children }: { children: ReactNode }) => {
-  return (
-    <ChannelsProvider soundsExtension="mp3" soundsPath="path/">
-      {children}
-    </ChannelsProvider>
-  );
-};
-
-describe('useChannels', () => {
-  it('Listens to main volume change', () => {
-    const { getByRole, getByTestId } = render(
-      <TestProviderComponent>
-        <ComponentThatChangesMainVolume />
-      </TestProviderComponent>
+describe('useVolumeChange', () => {
+  it('Listens to main volume changes', () => {
+    const wrapper = ({ children }: { children: ReactNode }) => (
+      <ChannelsProvider soundsExtension="mp3" soundsPath="path/">
+        {children}
+      </ChannelsProvider>
     );
-    expect(getByTestId('volume').textContent).toBe('1');
-    fireEvent.click(getByRole('button'));
-    expect(getByTestId('volume').textContent).toBe('0.5');
+
+    const onVolumeChange = jest.fn();
+    let channels: Channels;
+    renderHook(
+      () => {
+        channels = useChannels();
+        useVolumeChange({ onChange: onVolumeChange });
+      },
+      {
+        wrapper,
+      }
+    );
+
+    act(() => {
+      channels.setVolume(0.5);
+    });
+
+    expect(onVolumeChange).toHaveBeenCalledWith(0.5);
   });
 });
