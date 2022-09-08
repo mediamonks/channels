3f37a1a7b21d47efa5e8a2c00f25383dbf8ac7ac
Peter van der Noord <peterdunord@gmail.com>
2022-09-08 17:50:55 +0200

    Add test for useVolumeChange on a channel

--- packages/use-channels/src/useVolumeChange.test.tsx
+++ packages/use-channels/src/useVolumeChange.test.tsx
@@ -35,4 +35,30 @@ describe('useVolumeChange', () => {
 
     expect(onVolumeChange).toHaveBeenCalledWith(0.5);
   });
+  it('Listens to channel volume changes', () => {
+    const wrapper = ({ children }: { children: ReactNode }) => (
+      <ChannelsProvider soundsExtension="mp3" soundsPath="path/">
+        {children}
+      </ChannelsProvider>
+    );
+
+    const onVolumeChange = jest.fn();
+    let channels: Channels;
+    renderHook(
+      () => {
+        channels = useChannels();
+        const channel = channels.createChannel('channel');
+        useVolumeChange({ onChange: onVolumeChange, target: channel });
+      },
+      {
+        wrapper,
+      }
+    );
+
+    act(() => {
+      channels.getChannel('channel').setVolume(0.5);
+    });
+
+    expect(onVolumeChange).toHaveBeenCalledWith(0.5);
+  });
 });
