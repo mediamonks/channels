87e770f4da6833f96a2f79d36d3b00fcfda9bb30
Peter van der Noord <peterdunord@gmail.com>
2022-09-09 09:52:50 +0200

    Fix import for moved ChannelsProvider

--- packages/example-project/src/index.tsx
+++ packages/example-project/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChannelsProvider } from '@mediamonks/use-channels';
+import { ChannelsProvider } from '@mediamonks/use-channels/dist/ChannelsProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
